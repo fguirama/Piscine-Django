deae07d64b01f895f86f8f21371f7a23d0ee9e81
florian <florian.guiramand@protonmail.com>
2025-12-25 17:26:53 +0100

    make ex04

---
+++ django-0-starting/ex04/state.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def get_state(city):
+    states = {
+        "Oregon": "OR",
+        "Alabama": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver"
+    }
+
+    try:
+        code = get_key(capital_cities, city)
+        state = get_key(states, code)
+        return state
+    except ValueError:
+        return 'Unknown capital city'
+
+
+def get_key(dic, value):
+    for key, val in dic.items():
+        if val == value:
+            return key
+    raise ValueError()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        exit(1)
+    ipt_city = sys.argv[1]
+    print(get_state(ipt_city))
