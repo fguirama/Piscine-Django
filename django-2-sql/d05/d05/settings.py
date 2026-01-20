1ee714d61d980bd15e31ee98bc004f6a87585659
florian <florian.guiramand@protonmail.com>
2026-01-20 18:54:39 +0100

    add base template and update template directory in settings

--- django-2-sql/d05/d05/settings.py
+++ django-2-sql/d05/d05/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'd05.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['d05/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
