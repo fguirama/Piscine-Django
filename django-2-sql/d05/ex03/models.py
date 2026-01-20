3dc70a39a3cabd9424dc7d981ec984946b990c25
florian <florian.guiramand@protonmail.com>
2026-01-20 19:53:20 +0100

    add Movies model, data insertion, and display views for Exercice 03

--- django-2-sql/d05/ex03/models.py
+++ django-2-sql/d05/ex03/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Movies(models.Model):
+    episode_nb = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=64, unique=True, null=False)
+    opening_crawl = models.TextField(null=True)
+    director = models.CharField(max_length=32, null=False)
+    producer = models.CharField(max_length=128, null=False)
+    release_date = models.DateField(null=False)
+
+    def __str__(self):
+        return self.title
+
+
+def instert_sql_data():
+    movies = [
+        {'episode_nb': 1, 'title': 'The Phantom Menace', 'director': 'George Lucas', 'producer': 'Rick McCallum', 'release_date': '1999-05-19'},
+        {'episode_nb': 2, 'title': 'Attack of the Clones', 'director': 'George Lucas', 'producer': 'Rick McCallum', 'release_date': '2002-05-16'},
+        {'episode_nb': 3, 'title': 'Revenge of the Sith', 'director': 'George Lucas', 'producer': 'Rick McCallum', 'release_date': '2005-05-19'},
+        {'episode_nb': 4, 'title': 'A New Hope', 'director': 'George Lucas', 'producer': 'Gary Kurtz, Rick McCallum', 'release_date': '1977-05-25'},
+        {'episode_nb': 5, 'title': 'The Empire Strikes Back', 'director': 'Irvin Kershner', 'producer': 'Gary Kurtz, Rick McCallum', 'release_date': '1980-05-17'},
+        {'episode_nb': 6, 'title': 'Return of the Jedi', 'director': 'Richard Marquand', 'producer': 'Howard G. Kazanjian, George Lucas, Rick McCallum', 'release_date': '1983-05-25'},
+        {'episode_nb': 7, 'title': 'The Force Awakens', 'director': 'J. J. Abrams', 'producer': 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', 'release_date': '2015-12-11'}
+    ]
+
+    try:
+        for movie in movies:
+            Movies.objects.create(**movie)
+    except Exception as e:
+        return {'status': 'KO', 'text': f'Error inserting data: {e}'}
+
+    return {'status': 'OK', 'text': 'Data inserted successfully!'}
+
+
+def get_sql_data():
+    try:
+        return {'movies': Movies.objects.all().order_by('episode_nb'), 'status': 'OK'}
+    except Exception as e:
+        return {'movies': [], 'status': 'KO', 'text': f'Error fetching data: {e}'}
--- django-2-sql/d05/ex03/views.py
+++ django-2-sql/d05/ex03/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ex03.models import instert_sql_data, get_sql_data
+
+
+def ex03_populate(request):
+    context = instert_sql_data()
+    return render(request, 'status.html', context)
+
+
+def ex03_display(request):
+    data = get_sql_data()
+    print(data)
+    return render(request, 'display_movies.html', data)
