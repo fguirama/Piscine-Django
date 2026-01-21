c2cf22b39cae88beee6920f4ca46957285b874f3
florian <florian.guiramand@protonmail.com>
2026-01-21 20:12:39 +0100

    refactor remove_movie.html and models.py to display movie titles and improve error messaging

--- django-2-sql/d05/ex04/models.py
+++ django-2-sql/d05/ex04/models.py
@@ -4,6 +4,6 @@ def remove_sql_data(conn, cursor, n, title):
     if deleted_rows:
         context = {'status': 'OK', 'text': 'Data removed successfully!'}
     else:
-        context = {'status': 'KO', 'text': 'No data found to remove!'}
+        context = {'status': 'KO', 'text': f'No movie found with title "{title}"'}
     conn.commit()
     return context
