6e2c05f921cf6a299c81274c7fcf4efbaa79837f
florian <florian.guiramand@protonmail.com>
2026-01-21 19:15:30 +0100

    refactor movie table creation and initialization to use dynamic exercise numbers

--- django-2-sql/d05/ex00/models.py
+++ django-2-sql/d05/ex00/models.py
@@ -1,21 +1,21 @@
 from ex00.sql import connect_postgres
 
 
-def create_movies_table(exersice):
+def create_movies_table(n):
     conn = connect_postgres()
     if not conn:
         return {'status': 'KO', 'text': 'Error connecting to database'}
 
     try:
         with conn.cursor() as cursor:
-            cursor.execute(f"""CREATE TABLE IF NOT EXISTS ex0{exersice}_movies (
+            cursor.execute('''CREATE TABLE IF NOT EXISTS ex0%s_movies (
                 title VARCHAR(64) UNIQUE NOT NULL,
                 episode_nb INTEGER PRIMARY KEY,
                 opening_crawl TEXT,
                 director VARCHAR(32) NOT NULL,
                 producer VARCHAR(128) NOT NULL,
                 release_date DATE NOT NULL
-            );""")
+            );''', (n, ))
 
             conn.commit()
             context = {'status': 'OK', 'text': 'Table ‘ex00_movies’ successfully created!'}
--- django-2-sql/d05/ex00/views.py
+++ django-2-sql/d05/ex00/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from ex00.models import create_movies_table
 
 
-def ex00_init(request, exersice=0):
-    context = create_movies_table(exersice)
-    return render(request, 'status.html', context)
+def ex00_init(request, n=0):
+    context = create_movies_table(n)
+    return render_status(request, context, n)
+
+
+def render_status(request, context, n):
+    return render(request, 'status.html', {**context, 'n': n})
