544574f0838690c7bde94e61d75e666c6b60aa0f
florian <florian.guiramand@protonmail.com>
2026-01-21 20:12:58 +0100

    make exercise 05

--- django-2-sql/d05/ex05/views.py
+++ django-2-sql/d05/ex05/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ex03.models import get_orm_data
+from ex03.views import ex03_display, ex03_populate
+from ex05.models import Movies
+
+
+def ex05_populate(request):
+    return ex03_populate(request, 5, Movies)
+
+
+def ex05_display(request):
+    return ex03_display(request, 5, Movies)
+
+
+def ex05_remove(request, n=5, movies_database=Movies):
+    status = None
+    if request.method == 'POST':
+        title = request.POST.get('movie')
+        a = movies_database.objects.filter(title=title).delete()
+        if a[0]:
+            status = {'status': 'OK', 'text': 'Data removed successfully!'}
+        else:
+            status = {'status': 'KO', 'text': f'No movie found with title "{title}"'}
+
+    data = get_orm_data(movies_database)
+    print('ex05_remove', data, flush=True)
+    return render(request, 'remove_movie.html', {**data, 'n': n, 'status': status})
