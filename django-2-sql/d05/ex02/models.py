3ee2f4af81a9abad664a5848576787cb6825487a
florian <florian.guiramand@protonmail.com>
2026-01-21 19:15:26 +0100

    refactor SQL data handling to support dynamic exercise numbers in models and views

--- django-2-sql/d05/ex02/models.py
+++ django-2-sql/d05/ex02/models.py
@@ -3,7 +3,7 @@ from psycopg2.extras import DictCursor
 from ex00.sql import connect_postgres
 
 
-def instert_sql_data():
+def instert_sql_data(n):
     movies = [
         (1, 'The Phantom Menace', 'George Lucas', 'Rick McCallum', '1999-05-19'),
         (2, 'Attack of the Clones', 'George Lucas', 'Rick McCallum', '2002-05-16'),
@@ -14,13 +14,13 @@ def instert_sql_data():
         (7, 'The Force Awakens', 'J. J. Abrams', 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', '2015-12-11'),
     ]
 
-    try:
-        conn = connect_postgres()
-        if not conn:
-            return {'status': 'KO', 'text': 'Error connecting to database'}
+    conn = connect_postgres()
+    if not conn:
+        return {'status': 'KO', 'text': 'Error connecting to database'}
 
+    try:
         cur = conn.cursor()
-        query = """INSERT INTO ex02_movies (episode_nb, title, director, producer, release_date) VALUES (%s, %s, %s, %s, %s);"""
+        query = f'INSERT INTO ex0{n}_movies (episode_nb, title, director, producer, release_date) VALUES (%s, %s, %s, %s, %s);'
 
         for movie in movies:
             cur.execute(query, movie)
@@ -28,29 +28,31 @@ def instert_sql_data():
 
         cur.close()
         conn.close()
+        context = {'status': 'OK', 'text': 'Data inserted successfully!'}
 
     except Exception as e:
-        return {'status': 'KO', 'text': f'Error inserting data: {e}'}
+        context = {'status': 'KO', 'text': f'Error inserting data: {e}'}
+    finally:
+        conn.close()
+    return context
 
-    return {'status': 'OK', 'text': 'Data inserted successfully!'}
 
+def get_sql_data(n):
+    conn = connect_postgres()
+    if not conn:
+        return {'movies': [], 'status': 'KO', 'text': 'Error connecting to database'}
 
-def get_sql_data():
     try:
-        conn = connect_postgres()
-        if not conn:
-            return {'movies': [], 'status': 'KO', 'text': 'Error connecting to database'}
         cur = conn.cursor(cursor_factory=DictCursor)
 
-        query = """SELECT * FROM ex02_movies ORDER BY episode_nb;"""
-
-        cur.execute(query)
+        cur.execute('SELECT * FROM ex0%s_movies ORDER BY episode_nb;', (n, ))
         rows = cur.fetchall()
 
         cur.close()
         conn.close()
-
+        context = {'movies': rows, 'status': 'OK'}
     except Exception as e:
-        return {'movies': [], 'status': 'KO', 'text': f'Error fetching data: {e}'}
-
-    return {'movies': rows, 'status': 'OK'}
+        context = {'movies': [], 'status': 'KO', 'text': f'Error fetching data: {e}'}
+    finally:
+        conn.close()
+    return context
--- django-2-sql/d05/ex02/views.py
+++ django-2-sql/d05/ex02/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from ex00.views import ex00_init
+from ex00.views import ex00_init, render_status
 from ex02.models import instert_sql_data, get_sql_data
 
 
@@ -8,11 +8,11 @@ def ex02_init(request):
     return ex00_init(request, 2)
 
 
-def ex02_populate(request):
-    context = instert_sql_data()
-    return render(request, 'status.html', context)
+def ex02_populate(request, n=2):
+    context = instert_sql_data(n)
+    return render_status(request, context, n)
 
 
-def ex02_display(request):
-    data = get_sql_data()
+def ex02_display(request, n=2):
+    data = get_sql_data(n)
     return render(request, 'display_movies.html', data)
