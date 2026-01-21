67cc924899c201b8d2867c1014c13f95a417c01d
florian <florian.guiramand@protonmail.com>
2026-01-21 23:52:07 +0100

    feat: make fields in Planets and People models optional

--- django-2-sql/d05/ex09/models.py
+++ django-2-sql/d05/ex09/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Planets(models.Model):
     name = models.CharField(unique=True, max_length=64, null=False)
-    climate = models.TextField()
-    diameter = models.IntegerField()
-    orbital_period = models.IntegerField()
-    population = models.BigIntegerField()
-    rotation_period = models.IntegerField()
-    surface_water = models.FloatField()
-    terrain = models.TextField()
+    climate = models.TextField(null=True, blank=True)
+    diameter = models.IntegerField(null=True, blank=True)
+    orbital_period = models.IntegerField(null=True, blank=True)
+    population = models.BigIntegerField(null=True, blank=True)
+    rotation_period = models.IntegerField(null=True, blank=True)
+    surface_water = models.FloatField(null=True, blank=True)
+    terrain = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -19,13 +19,13 @@ class Planets(models.Model):
 
 class People(models.Model):
     name = models.CharField(unique=True, max_length=64, null=False)
-    birth_year = models.CharField(max_length=32)
-    gender = models.CharField(max_length=32)
-    eye_color = models.CharField(max_length=32)
-    hair_color = models.CharField(max_length=32)
-    height = models.IntegerField()
-    mass = models.FloatField()
-    homeworld = models.ForeignKey(Planets, on_delete=models.CASCADE)
+    birth_year = models.CharField(max_length=32, null=True, blank=True)
+    gender = models.CharField(max_length=32, null=True, blank=True)
+    eye_color = models.CharField(max_length=32, null=True, blank=True)
+    hair_color = models.CharField(max_length=32, null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    mass = models.FloatField(null=True, blank=True)
+    homeworld = models.ForeignKey(Planets, on_delete=models.CASCADE, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
