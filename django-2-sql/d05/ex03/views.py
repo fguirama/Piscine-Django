dfb26a728c14b6ab8e6f37c644264e887f00023c
florian <florian.guiramand@protonmail.com>
2026-01-21 19:15:21 +0100

    refactor ex03 views to use render_status for context rendering

--- django-2-sql/d05/ex03/views.py
+++ django-2-sql/d05/ex03/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 
+from ex00.views import render_status
 from ex03.models import instert_sql_data, get_sql_data
 
 
 def ex03_populate(request):
     context = instert_sql_data()
-    return render(request, 'status.html', context)
+    return render_status(request, context, 3)
 
 
 def ex03_display(request):
     data = get_sql_data()
-    print(data)
     return render(request, 'display_movies.html', data)
