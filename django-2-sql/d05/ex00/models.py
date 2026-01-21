8c9cccbc4fb2bf5228c7e904c2b5822bcb9997e2
florian <florian.guiramand@protonmail.com>
2026-01-21 21:17:13 +0100

    make ex06

--- django-2-sql/d05/ex00/models.py
+++ django-2-sql/d05/ex00/models.py
@@ -1,11 +1,29 @@
 def create_movies_table(conn, cursor, n):
-    cursor.execute('''CREATE TABLE IF NOT EXISTS ex0%s_movies (
+    extra_fields = ''
+    if n == 6:
+        extra_fields = ''',
+        created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP'''
+    cursor.execute(f'''CREATE TABLE IF NOT EXISTS ex0%s_movies (
         title VARCHAR(64) UNIQUE NOT NULL,
         episode_nb INTEGER PRIMARY KEY,
         opening_crawl TEXT,
         director VARCHAR(32) NOT NULL,
         producer VARCHAR(128) NOT NULL,
-        release_date DATE NOT NULL
+        release_date DATE NOT NULL {extra_fields}
     );''', (n, ))
+    if n == 6:
+        cursor.execute('''CREATE OR REPLACE FUNCTION update_changetimestamp_column()
+            RETURNS TRIGGER AS $$
+            BEGIN
+            NEW.updated = now();
+            NEW.created = OLD.created;
+            RETURN NEW;
+            END;
+            $$ language 'plpgsql';
+            CREATE TRIGGER update_films_changetimestamp BEFORE UPDATE
+            ON ex06_movies FOR EACH ROW EXECUTE PROCEDURE
+            update_changetimestamp_column();
+        ''')
     conn.commit()
-    return {'status': 'OK', 'text': 'Table ‘ex00_movies’ successfully created!'}
+    return {'status': 'OK', 'text': f'Table ‘ex0{n}_movies’ successfully created!'}
--- django-2-sql/d05/ex06/models.py
+++ django-2-sql/d05/ex06/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
-# Create your models here.
+def update_sql_data(conn, cursor, n, title, opening_crawl):
+    cursor.execute('UPDATE ex0%s_movies SET opening_crawl = %s WHERE title = %s;', (n, opening_crawl, title))
+    updated_rows = cursor.rowcount
+    if updated_rows:
+        context = {'status': 'OK', 'text': 'Data updated successfully!'}
+    else:
+        context = {'status': 'KO', 'text': f'No movie found with title "{title}"'}
+    conn.commit()
+    return context
