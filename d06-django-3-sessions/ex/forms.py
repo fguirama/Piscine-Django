b382c752a40740b99251186714b476f02da55878
florian <florian.guiramand@protonmail.com>
2026-01-23 20:39:18 +0100

    make d06/forms.py

---
+++ d06-django-3-sessions/ex/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('Username already exists.')
+        return username
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm = cleaned_data.get('password_confirm')
+
+        if password and confirm and password != confirm:
+            self.add_error('password_confirm', 'Passwords do not match.')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('Invalid credentials.')
+            self.user = user
