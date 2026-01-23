01753cea6b28aa27b0193f58338a2ddec1c5efc4
florian <florian.guiramand@protonmail.com>
2026-01-23 20:47:56 +0100

    fix: rename index view URL path to 'home' for clarity

--- d06-django-3-sessions/ex/urls.py
+++ d06-django-3-sessions/ex/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ex import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    path('', views.index_view, name='home'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
