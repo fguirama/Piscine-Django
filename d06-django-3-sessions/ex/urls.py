46b555a27e575c48ee5be297c588e05477c3a671
florian <florian.guiramand@protonmail.com>
2026-01-23 20:39:29 +0100

    feat: add URL routing for index, signup, login, and logout views

---
+++ d06-django-3-sessions/ex/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from ex import views
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
