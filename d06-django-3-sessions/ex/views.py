9fde649de480106a4f1a77db27309ad6fd5b5b22
florian <florian.guiramand@protonmail.com>
2026-01-26 19:04:51 +0100

    feat: implement upvote, downvote, and delete functionality for tips with permission checks

--- d06-django-3-sessions/ex/views.py
+++ d06-django-3-sessions/ex/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect, get_object_or_404
 
 from ex.forms import SignupForm, LoginForm, TipsForm
-from ex.models import Tip
+from ex.models import Tip, User
 
 
 def index_view(request):
@@ -20,18 +21,20 @@ def index_view(request):
                 return redirect('home')
         else:
             form = TipsForm()
-    return render(request, 'home.html', {'tips': tips, 'form': form})
+    return render(request, 'home.html', {'tips': tips, 'form': form, 'can_delete': request.user.has_perm('ex.delete_tip'), 'can_downvote': request.user.has_perm('ex.can_downvote_tip')})
 
 
 def login_view(request):
+    next_url = request.GET.get('next', 'home')
+
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect(next_url)
 
     form = LoginForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
         login(request, form.user)
-        return redirect('home')
+        return redirect(next_url)
 
     return render(request, 'login.html', {'form': form})
 
@@ -42,14 +45,48 @@ def logout_view(request):
 
 
 def signup_view(request):
+    next_url = request.GET.get('next', 'home')
+
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect(next_url)
 
     form = SignupForm(request.POST or None)
 
     if request.method == 'POST' and form.is_valid():
         user = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
         login(request, user)
-        return redirect('home')
+        return redirect(next_url)
 
     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def upvote_downvote_tip(request, tip_id):
+    tip = get_object_or_404(Tip, id=tip_id)
+    user = request.user
+
+    action = 'upvotes' if request.resolver_match.url_name is 'upvote_tip' else 'downvotes'
+    if action == 'upvotes' or (user == tip.author or user.has_perm('ex.can_downvote_tip')):
+        a = getattr(tip, action)
+        if user in a.all():
+            a.remove(user)
+        else:
+            oposite_action = 'downvotes' if action is 'upvotes' else 'upvotes'
+            o = getattr(tip, oposite_action)
+            if user in o.all():
+                o.remove(user)
+            a.add(user)
+
+    raise PermissionDenied
+
+
+@login_required
+def delete_tip(request, tip_id):
+    tip = get_object_or_404(Tip, id=tip_id)
+    user = request.user
+
+    if user == tip.author or user.has_perm('ex.delete_tip'):
+        tip.delete()
+        return redirect('home')
+
+    raise PermissionDenied
