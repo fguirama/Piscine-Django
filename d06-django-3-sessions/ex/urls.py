e767a86cbbf05c66fe849fe116c5be0024278ef3
florian <florian.guiramand@protonmail.com>
2026-01-26 19:04:46 +0100

    feat: add upvote, downvote, and delete routes for tips in urls.py

--- d06-django-3-sessions/ex/urls.py
+++ d06-django-3-sessions/ex/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('upvote/<int:tip_id>/', views.upvote_downvote_tip, name='upvote_tip'),
+    path('downvote/<int:tip_id>/', views.upvote_downvote_tip, name='downvote_tip'),
+    path('delete/<int:tip_id>/', views.delete_tip, name='delete_tip'),
 ]
