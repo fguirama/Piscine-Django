e44c600f63f136cb3d126e2b335881adc892da14
florian <florian.guiramand@protonmail.com>
2026-02-05 20:30:38 +0100

    add features add to favorite

--- d07-django-3-advanced/articles/urls.py
+++ d07-django-3-advanced/articles/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from articles.views import ArticleListView, PublicationListView, ArticleDetailView, FavouriteListView, ArticleCreateView
+from articles.views import ArticleListView, PublicationListView, ArticleDetailView, FavouriteListView, \
+    ArticleCreateView, AddFavouriteView
 
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='articles'),
-    path('publications/', PublicationListView.as_view(), name='publications'),
     path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('articles/<int:pk>/favourite/', AddFavouriteView.as_view(), name='add-favourite'),
     path('favourites/', FavouriteListView.as_view(), name='favourites'),
+    path('publications/', PublicationListView.as_view(), name='publications'),
     path('publish/', ArticleCreateView.as_view(), name='publish')
 ]
--- d07-django-3-advanced/articles/views.py
+++ d07-django-3-advanced/articles/views.py
@@ -44,3 +44,17 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class AddFavouriteView(LoginRequiredMixin, CreateView):
+    model = UserFavouriteArticle
+    fields = []
+    template_name = 'add_favourite.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.article_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('article-detail', kwargs={'pk': self.kwargs['pk']})
