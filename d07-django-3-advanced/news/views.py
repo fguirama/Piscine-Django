e71c4369c9917ff5368dcc9084a534f00d9f99ad
florian <florian.guiramand@protonmail.com>
2026-02-15 18:50:11 +0100

    feat: redirect authenticated users from registration to home

--- d07-django-3-advanced/news/views.py
+++ d07-django-3-advanced/news/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -10,6 +11,11 @@ class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.save(commit=False)
