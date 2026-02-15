6cea95f61c149b1b8b987ad7eac4b470d09b653d
florian <florian.guiramand@protonmail.com>
2026-02-15 20:05:12 +0100

    feat: add base HTML template and configure static files in settings

--- d08-django-3-final/d09/settings.py
+++ d08-django-3-final/d09/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'd09.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['d09/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
