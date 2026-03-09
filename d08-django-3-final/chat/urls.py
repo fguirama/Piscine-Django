4f9f05463b4d25608677ca5271236fdf8cf42a41
florian <florian.guiramand@protonmail.com>
2026-03-09 15:26:57 +0100

    feat: add chat detail view and update chats listing with links

--- d08-django-3-final/chat/urls.py
+++ d08-django-3-final/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chat.views import chats_view
+from chat.views import chat_view, chats_view
 
 urlpatterns = [
-    path('', chats_view)
+    path('', chats_view, name='chat'),
+    path('<int:chat_id>/', chat_view, name='chats')
 ]
--- d08-django-3-final/chat/views.py
+++ d08-django-3-final/chat/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from chat.models import Chat
 
 
 def chats_view(request):
-    return render(request, 'chats.html')
+    chats = Chat.objects.all()
+    return render(request, 'chats.html', context={'chats': chats})
+
+
+def chat_view(request, chat_id):
+    chat = get_object_or_404(Chat, pk=chat_id)
+    return render(request, 'chat.html', context={'chat': chat})
