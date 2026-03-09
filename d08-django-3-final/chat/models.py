80e31896cdde58bc1c51fb6d336a5fb36addbc67
florian <florian.guiramand@protonmail.com>
2026-03-09 15:26:20 +0100

    feat: add Chat model and register it in admin

--- d08-django-3-final/chat/models.py
+++ d08-django-3-final/chat/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    name = models.CharField(max_length=255)
+    users = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.name
