c0e00989ef6eb29fd2fb8c1485c5485f49d2d050
florian <florian.guiramand@protonmail.com>
2026-03-13 18:16:35 +0100

    feat: enhance message handling in chatroom.js and consumers.py

--- d08-django-3-final/chat/consumers.py
+++ d08-django-3-final/chat/consumers.py
@@ -18,26 +18,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = None
 
     async def connect(self, test=None):
-        print('CONNECT', test, flush=True)
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         if not await room_exists(self.room_name):
             await self.close()
+
         self.room_group_name = f'chat_{self.room_name}'
+        username = self.scope["user"].username
 
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'connect',
-        #         'room_name': self.room_name
-        #     }
-        # )
-
         await self.accept()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'room_name': self.room_name,
+                'message': f'{username} has joined the chat'
+            }
+        )
 
     async def disconnect(self, close_code):
 
@@ -56,7 +58,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        print(data, flush=True)
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -68,8 +69,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def chat_message(self, event):
-        print('CHAT MESSAGE', flush=True)
-        await self.send(text_data=json.dumps({
+        data = {
             'message': event['message'],
-            'username': event['username']
-        }))
+        }
+        if event.get('username'):
+            data['username'] = event['username']
+
+        await self.send(text_data=json.dumps(data))
--- d08-django-3-final/chat/static/chat/chatroom.js
+++ d08-django-3-final/chat/static/chat/chatroom.js
@@ -1,32 +1,21 @@
 function showMessage(data) {
     const $listMessages = $('<li>');
 
-    $listMessages.addClass('d-flex');
-    $listMessages.append($('<div>').text(data.username + ':'))
-    $listMessages.append($('<div>').text(data.message))
+    if (data.username) {
+        $listMessages.addClass('d-flex');
+        $listMessages.append($('<div>').text(data.username + ':'))
+        $listMessages.append($('<div>').text(data.message))
+    }
+    else {
+        $listMessages.text(data.message)
+    }
     $('#messages').append($listMessages)
 }
 
+
+
 $(document).ready(function() {
-    console.log('Room name:', roomName);
-    const socket = new WebSocket('ws://' + window.location.host + '/ws/chat/' + roomName + '/');
-
-    socket.onopen = function() {
-        console.log('Connected');
-    };
-
-    socket.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-        console.log('Message:', data);
-
-        showMessage(data);
-    };
-
-    socket.onclose = function() {
-        console.log('Disconnected');
-    };
-
-    $('#send').click(function() {
+    function sendMessage() {
         const $message = $('#message');
         const message = $message.val();
         const data = {
@@ -37,5 +26,32 @@ $(document).ready(function() {
         $message.val('');
         socket.send(JSON.stringify(data));
         $message.focus();
+    }
+
+    const socket = new WebSocket('ws://' + window.location.host + '/ws/chat/' + roomName + '/');
+
+    socket.onopen = function() {
+        console.log('Connected');
+    };
+
+    socket.onmessage = function(event) {
+        const data = JSON.parse(event.data);
+
+        showMessage(data);
+    };
+
+    socket.onclose = function() {
+        console.log('Disconnected');
+    };
+
+    $('#message').on('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            sendMessage();
+        }
+    });
+
+    $('#send').click(function() {
+        sendMessage();
     });
 });
