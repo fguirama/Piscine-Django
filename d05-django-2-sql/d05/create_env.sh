169d21bc48a24cc6bd9efc73c48f9aaa8756c31f
florian <florian.guiramand@protonmail.com>
2026-03-20 18:11:38 +0100

    fix: correct requirements file name in environment setup script

--- d05-django-2-sql/d05/create_env.sh
+++ d05-django-2-sql/d05/create_env.sh
@@ -4,6 +4,6 @@ python3 -m venv django_venv
 source django_venv/bin/activate
 
 pip install --upgrade pip
-pip install -r requirement.txt
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py runserver
